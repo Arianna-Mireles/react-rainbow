b94d6475df89466f4c8dbc140bbef992c962750a
Arianna Mireles <arianna.mireles02@gmail.com>
2023-11-18 21:28:19 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+import React, { useState } from 'react'
+
+function App(){
+    let colors = ['violet', 'blue', 'lightblue', 'green', 'greenyellow', 'yellow', 'orange', 'red']
+    return (
+        <div className="App">
+
+        </div>
+    )
+}
+
+
+export default App;
